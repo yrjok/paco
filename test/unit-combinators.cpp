1f9ad68933ff07d9009b906a1e912d6a919afe8e
Yrjo <yrjo@live.be>
2022-12-23 10:16:05 +0100

    Update combinator tests

--- test/unit-combinators.cpp
+++ test/unit-combinators.cpp
@@ -9,39 +9,77 @@
 using namespace paco;
 
 TEST_SUITE("Combinators") {
+  TEST_CASE("Satisying") {
+    auto base = fundamentals::any();
+    auto predicate = [](std::string_view v) {
+      return v.size() == 0 or v.front() >= 'a' and v.front() <= 'z';
+    };
+    auto matcher = satisfying(base, predicate);
+
+    SUBCASE("Matches when both the underlying matcher and the predicate matches") {
+      std::string text = "word";
+      REQUIRE(base.matches(text));
+      REQUIRE(predicate(text));
+      CHECK(matcher.matches(text));
+    }
+
+    SUBCASE("does not match when the underlying matcher does not match") {
+      std::string text = "";
+      REQUIRE(not base.matches(text));
+      REQUIRE(predicate(text));
+      CHECK(not matcher.matches(text));
+    }
+
+    SUBCASE("does not match when the predicate does not match") {
+      std::string text = "WORD";
+      REQUIRE(base.matches(text));
+      REQUIRE(not predicate(text));
+      CHECK(not matcher.matches(text));
+    }
+  }
+
   TEST_CASE("Either") {
-    auto base = std::make_unique<fundamentals::any>();
-    satisfying first(base->clone(), [](auto v) { return v == "a"; });
-    satisfying second(base->clone(), [](auto v) { return v == "b"; });
+    auto base = fundamentals::any();
+    auto low = [](std::string_view v) { return v.front() >= 'a' and v.front() <= 'b'; };
+    auto high = [](std::string_view v) { return v.front() >= 'b' and v.front() <= 'c'; };
+    satisfying first(base, low);
+    satisfying second(base, high);
 
     either recognizer(first.clone(), second.clone());
 
-    SUBCASE("Matches using the first recognizer") {
-      std::string content("abcd");
+    SUBCASE("Matches when only the first recognizer matches") {
+      std::string content("alice");
       REQUIRE(first.matches(content));
       REQUIRE(not second.matches(content));
       CHECK(recognizer.matches(content));
     }
 
-    SUBCASE("Matches using the second recognizer") {
-      std::string content("bcd");
+    SUBCASE("Matches when only the second recognizer matches") {
+      std::string content("combine");
       REQUIRE(not first.matches(content));
       REQUIRE(second.matches(content));
       CHECK(recognizer.matches(content));
     }
 
-    SUBCASE("Does not match anything else") {
-      std::string content("cd");
+    SUBCASE("Does not match when none of both match") {
+      std::string content(":)");
       REQUIRE(not first.matches(content));
       REQUIRE(not second.matches(content));
       CHECK(not recognizer.matches(content));
     }
+
+    SUBCASE("Matches when both match") {
+      std::string content("bob");
+      REQUIRE(first.matches(content));
+      REQUIRE(second.matches(content));
+      CHECK(recognizer.matches(content));
+    }
   }
 
   TEST_CASE("Sequence") {
-    auto base = std::make_unique<fundamentals::any>();
-    satisfying first(base->clone(), [](auto v) { return v == "a"; });
-    satisfying second(base->clone(), [](auto v) { return v == "b"; });
+    auto base = fundamentals::any();
+    satisfying first(base, [](auto v) { return v == "a"; });
+    satisfying second(base, [](auto v) { return v == "b"; });
 
     sequence recognizer(first.clone(), second.clone());
 
@@ -68,8 +106,8 @@ TEST_SUITE("Combinators") {
   }
 
   TEST_CASE("Many") {
-    auto base = std::make_unique<fundamentals::any>();
-    satisfying first(base->clone(), [](auto v) { return v == "y"; });
+    auto base = fundamentals::any();
+    satisfying first(base, [](auto v) { return v == "y"; });
     many recognizer(first.clone());
 
     SUBCASE("Matches a sequences of as many matching parts as possible") {
